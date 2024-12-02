373d7650900f0431989075f2d445bb7a4c45dfb2
Yub-S <ys7887811@gmail.com>
2024-12-02 19:42:47 +0545

    config changed

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-    API_KEY: "AIzaSyBu2fiNVUtrzKye_iItTusqtVLVHGgPqZY",
+    API_KEY: "your api key here",
     API_URL: "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent"
 };
 
